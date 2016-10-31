b63d7a6f1cc2f4266f63bfc5ab6a00ceb3d181c9
Stefano Calza <stefano.calza@unibs.it>
2016-10-31 15:56:37 +0100

    set custom load data name to dataset

--- inst/app/server.R
+++ inst/app/server.R
@@ -31,7 +31,7 @@ shinyServer(function(input, output, session) {
       ## Check if the data file is in radiant.exams
       df_name = ifelse(query[["code"]] %in% data.in.pkg, query[["code"]], "titanic")
 	  df <- data(list = df_name, package = "radiant.exams", envir = environment()) %>% get
-	  df_name = 'dataset'
+	  df_name = paste('data',sample(c(letters,LETTERS,0:9),10),collapse="")
 	  
     } else { 
 		df_name <- getOption("radiant.init.data", default = "titanic")
