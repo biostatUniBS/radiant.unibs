7fd17fbd0ab74dccac6455ffd2fb053062ce214c
Stefano Calza <stefano.calza@unibs.it>
2018-06-10 19:56:45 +0200

    global.R

--- inst/app/global.R
+++ inst/app/global.R
@@ -72,3 +72,115 @@ options(radiant.shared_ui =
           )
 )
 
+
+## from radian.data -> manage_ui.R
+
+output$ui_Manage <- renderUI({
+  data_types_in <- c(
+    "rds" = "rds", "rda (rdata)" = "rda", "csv" = "csv",
+    "clipboard" = "clipboard", "examples" = "examples",
+    "rda (url)" = "url_rda", "csv (url)" = "url_csv",
+    "feather" = "feather", "from global workspace" = "from_global",
+    "radiant state file" = "state" 
+  )
+  data_types_out <- c(
+    "rds" = "rds", "rda" = "rda", "csv" = "csv",
+    "clipboard" = "clipboard", "feather" = "feather",
+    "to global workspace" = "to_global", "radiant state file" = "state"
+  )
+  if (!isTRUE(getOption("radiant.local"))) {
+    data_types_in <- data_types_in[-which(data_types_in == "from_global")]
+    data_types_out <- data_types_out[-which(data_types_out == "to_global")]
+  }
+  if (!("feather" %in% rownames(utils::installed.packages()))) {
+    data_types_in <- data_types_in[-which(data_types_in == "feather")]
+    data_types_out <- data_types_out[-which(data_types_out == "feather")]
+  }
+
+  tagList(
+    wellPanel(
+      selectInput("dataType", label = "Load data of type:", data_types_in, selected = "rds"),
+      conditionalPanel(
+        condition = "input.dataType != 'clipboard' &&
+                     input.dataType != 'examples'",
+        conditionalPanel(
+          "input.dataType == 'csv' | input.dataType == 'url_csv'",
+          with(tags, table(
+            td(checkboxInput("man_header", "Header", TRUE)),
+            td(HTML("&nbsp;&nbsp;")),
+            td(checkboxInput("man_str_as_factor", "Str. as Factor", TRUE))
+          )),
+          with(tags, table(
+            td(selectInput("man_sep", "Separator:", c(Comma = ",", Semicolon = ";", Tab = "\t"), ",", width = "100%")),
+            td(selectInput("man_dec", "Decimal:", c(Period = ".", Comma = ","), ".", width = "100%")),
+            width = "100%"
+          )),
+          numericInput(
+            "man_n_max", label = "Maximum rows to read:",
+            value = Inf, max = Inf, step = 1000
+          )
+        ),
+        uiOutput("ui_fileUpload")
+      ),
+      conditionalPanel(
+        condition = "input.dataType == 'clipboard'",
+        uiOutput("ui_clipboard_load")
+      ),
+      conditionalPanel(
+        condition = "input.dataType == 'from_global'",
+        uiOutput("ui_from_global")
+      ),
+      conditionalPanel(
+        condition = "input.dataType == 'examples'",
+        actionButton("loadExampleData", "Load", icon = icon("upload"))
+      ),
+      conditionalPanel(
+        condition = "input.dataType == 'state'",
+        uiOutput("ui_state_load"),
+        uiOutput("refreshOnUpload")
+      )
+    ),
+    #wellPanel(
+    #  selectInput("saveAs", label = "Save data to type:", data_types_out, selected = "rds"),
+    #  conditionalPanel(
+    #    condition = "input.saveAs == 'clipboard'",
+    #    uiOutput("ui_clipboard_save")
+    #  ),
+    #  conditionalPanel(
+    #    condition = "input.saveAs == 'state'",
+    #    HTML("<label>Save radiant state file (.rda):</label><br/>"),
+    #    uiOutput("ui_man_state_save")
+    #  ),
+    #  conditionalPanel(
+    #    condition = "input.saveAs == 'to_global'",
+    #    uiOutput("ui_to_global")
+    #  ),
+    #  conditionalPanel(
+    #    condition = "input.saveAs != 'clipboard' &&
+    #                 input.saveAs != 'state' &&
+    #                 input.saveAs != 'to_global'",
+    #    uiOutput("ui_man_save_data")
+    #  )
+    #),
+    # conditionalPanel(
+      # "output.is_browser == false",
+      #wellPanel(
+      #  checkboxInput("man_show_log", "Show R-code", FALSE)
+      #),
+    # ),
+    wellPanel(
+      checkboxInput("man_show_remove", "Remove data from memory", FALSE),
+      conditionalPanel(
+        condition = "input.man_show_remove == true",
+        uiOutput("uiRemoveDataset"),
+        actionButton("removeDataButton", "Remove data", icon = icon("trash"), class = "btn-danger")
+      )
+    ),
+    help_and_report(
+      modal_title = "Manage",
+      fun_name = "manage",
+      help_file = inclMD(file.path(getOption("radiant.path.data"), "app/tools/help/manage.md")),
+      lic = "by-sa"
+    )
+  )
+})
